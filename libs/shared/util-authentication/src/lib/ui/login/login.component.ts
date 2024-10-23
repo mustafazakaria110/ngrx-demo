1e23f772e4a8d38f8102c78837ac58ed8c9e41a6
sara.magdy <sara.magdy@rosenfieldhealth.com>
2024-10-23 10:04:57 +0300

    commit

--- libs/shared/util-authentication/src/lib/ui/login/login.component.ts
+++ libs/shared/util-authentication/src/lib/ui/login/login.component.ts
@@ -6,9 +6,9 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
 
-  
+
 }

4165a3423f2f6dc5d3bd099b9d1f337b8712d4d6
sara.magdy <sara.magdy@rosenfieldhealth.com>
2024-10-27 10:20:22 +0300

    edit user

--- apps/APIs/APIs/APIs/Controllers/UsersController.cs
+++ apps/APIs/APIs/APIs/Controllers/UsersController.cs
@@ -78,8 +78,6 @@ namespace APIs.Controllers
     {
       try
       {
-        if (user.UserRole == 0)
-          user.UserRole = 2;
         var command = new AddUserCommand()
         {
           User = user

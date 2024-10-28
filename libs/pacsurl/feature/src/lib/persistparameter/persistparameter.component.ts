73790a25b9b0769025b6fa75709d0fb4d69ac822
ahmed.galbat@rosenfieldhealth.com <ahmed.galbat@rosenfieldhealth.com>
2024-10-28 10:20:54 +0300

    fix

--- libs/pacsurl/feature/src/lib/persistparameter/persistparameter.component.ts
+++ libs/pacsurl/feature/src/lib/persistparameter/persistparameter.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ParameterComponent } from '@icode-tfs-ngrx-demo/pacsurl-ui';
 
 @Component({
   selector: 'lib-persitparameter',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,ParameterComponent],
   templateUrl: './persistparameter.component.html',
   styleUrl: './persistparameter.component.scss',
 })
